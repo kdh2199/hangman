c0ccd99b341f5333e96af9007cd84c0c2dcb2f62
kdh2199 <68812665+kdh2199@users.noreply.github.com>
2023-10-20 01:07:47 +0900

    master-path.py

--- game/path.py
+++ game/path.py
@@ -13,9 +13,9 @@ def get_path():
     # 현재 작업 폴더에 'game' 폴더가 있는 경우 (텍스트 파일이 'game' 폴더 안에 들어있는 경우) 경로 설정
     elif 'game' in file_list:
         path = './game/text.txt'
-    # 현재 작업 폴더에 'hangman-main' 폴더가 있는 경우 (텍스트 파일이 'hangman-main' 폴더 안에 들어있는 경우) 경로 설정
-    elif 'hangman-main' in file_list:
-        path = './hangman-main/game/text.txt'
+    # 현재 작업 폴더에 'hangman-master' 폴더가 있는 경우 (텍스트 파일이 'hangman-master' 폴더 안에 들어있는 경우) 경로 설정
+    elif 'hangman-master' in file_list:
+        path = './hangman-master/game/text.txt'
     # 이외에 텍스트 파일이나 폴더를 찾을 수 없는 경우 현재 작업 폴더 경로 반환
     else:
         path = now_path , file_list
